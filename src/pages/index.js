dafd78a5f5030afaff1781179f7db5259213d93e
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-15 18:35:57 +0100

    Create pages and Edit file - Create contact, portfolio and layout page - Add comment to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 
+// This is the index file for the homepage
+
 const index = () => {
   return (
     <div className='bg-black h-screen'>
