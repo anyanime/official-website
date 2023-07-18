2f289f27c69ef8b2e886f56a07167f24251bbee9
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-18 13:20:36 +0100

    Create default content for pages

---
+++ src/components/Copyright.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Copyright = props => {
+  return (
+    <p {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
+  )
+}
+
+export default Copyright
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <Layout>
+        <p>I am a very bad boy</p>
+    </Layout>
+  )
+}
+
+export default contact
