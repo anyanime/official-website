4ea27fabefaf4c127262bd21d0bad4df4d359e91
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-20 20:21:06 +0100

    create a reusable nav link

---
+++ src/components/NavLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const NavLink = ({children, to}) => {
+  return (
+    <Link to={to}>
+      {children}
+      </Link>
+  )
+}
+
+export default NavLink
