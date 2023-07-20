d65cb248696a07f548620b69740e533f565da787
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-20 20:44:16 +0100

    Add link to about button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import './Button.css'
+import {Link } from 'gatsby'
 
 const Button = () => {
   return (
     <div className='text-white text-center p-2 text-2xl mt-10 bg-blue uppercase rounded-full hover:bg-dark-blue'>
-      <button>More About Me</button>
+      <button><Link to='/about'>More About Me</Link></button>
     </div>
   )
 }
