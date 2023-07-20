a5bff99a34043cec0f0536441914456595cb7c4d
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-20 20:21:50 +0100

    Create component

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Copyright = props => {
   return (
-    <p {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
+    <p className="flex justify-end items-baseline"{...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
   )
 }
 
