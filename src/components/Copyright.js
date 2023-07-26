ac4195e1e2d03cd9aa299caf8909e2137b08175b
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-27 00:56:55 +0100

    Center and color text

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Copyright = props => {
   return (
-    <p className="flex justify-end items-baseline"{...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
+    <p className="flex justify-center text-white" {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
   )
 }
 
