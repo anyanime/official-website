a5e58fbbff067ca9209e42d714a87fd5d8db2330
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-27 00:56:00 +0100

    Style button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import {Link } from 'gatsby'
 
 const Button = () => {
   return (
-    <div className='text-white text-center p-2 text-2xl mt-10 bg-blue uppercase rounded-full hover:bg-dark-blue'>
+    <div className='text-white text-center p-2 text-2xl mt-10 bg-bh-btn uppercase rounded-full hover:bg-bh-lime animate-pulse'>
       <button><Link to='/about'>More About Me</Link></button>
     </div>
   )
