d4a2dfd247b034800f8d0f7a6b1f0267bbb1dcac
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-27 21:06:39 +0100

    Change landing page theme and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,13 @@ const index = () => {
   return (
     <div className='bg-black h-full max-sm-auto md:h-screen lg:h-screen'>
       <Header />
-      <div className='flex justify-center items-center h-5/6 flex-col-reverse gap-9 text-white p-7 font-semibold md:flex-row'>
+      <div className='absolute rounded-full bg-bh-yellow animate-spin w-10 h-30'>
+        <div className='rounded-full bg-bh-lime animate-spin w-12 h-32' ></div>
+      </div>
+      <div className='relative flex h-6 w-6'>
+        <div className='animate-ping absolute inline-flex w-full rounded-full bg-bh-lime top-10 opacity-75' ></div>
+      </div>
+      <div className='flex justify-center items-center gap-x-6 h-5/6 flex-col-reverse text-white p-6 font-semibold md:flex-row'>
         <div className='justify-start items-center p-6 lg:w-3/12'>
           <h1 className='font-medium text-5xl'>Hello!</h1>
           <h3 className='text-2xl'>I am Anyanime Benson</h3>
@@ -22,12 +28,15 @@ const index = () => {
             to bring about solutions to problem while maintaining 
             a fun and productive work environment.</p>
           <div>
-          <Button />  
+          <Button type='internal' to='/about'>More About Me</Button> 
           <SocialMediaIcon />
           </div>
         </div>
-        <div className='flex justify-center mx-10'>
-          <img className='h-96 rounded-lg ' src={image} alt='Protrait of Anyanime Benson'/>
+        <div>
+          <img className='h-full w-96 rounded-full justify-end' src={image} alt='Protrait of Anyanime Benson'/>
+          <div className='relative rounded-full bg-bh-lime animate-spin w-10 h-30'>
+            <div className='rounded-full bg-bh-red animate-spin w-12 h-32' ></div>
+           </div>
         </div>
       </div>
       <Copyright />
