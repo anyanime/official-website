a2a1ab8628771d318c1f1580066a46d49d24a484
Anyanime Benson <bensonanyanime@gmail.com>
2023-07-27 01:06:59 +0100

    adjust margin top of the copywright component

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Copyright = props => {
   return (
-    <p className="flex justify-center text-white" {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
+    <p className="flex justify-center text-white -mt-6" {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
   )
 }
 
