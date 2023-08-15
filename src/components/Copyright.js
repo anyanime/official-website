51d04f3cf071fe000baa79d9bb60f4de42720714
Anyanime Benson <bensonanyanime@gmail.com>
2023-08-15 15:58:36 +0100

    remove top margin styling and add text size

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Copyright = props => {
   return (
-    <p className="flex justify-center text-white -mt-6" {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
+    <p className="flex justify-center text-white md:text-xl " {...props}>Copyright &copy; {new Date().getFullYear()} Benson Anyanime</p>
   )
 }
 
