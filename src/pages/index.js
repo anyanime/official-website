05a1049994a098332c3645a44e48b98cdb616706
Anyanime Benson <bensonanyanime@gmail.com>
2023-08-18 19:23:27 +0100

    add font size for extra-large screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,11 @@ import Copyright from '../components/Copyright';
 
 const index = () => {
   return (
-    <div className='bg-black h-full sm:h-screen md:h-screen lg:h-screen xl:h-screen p-3'>
+    <div className='bg-black text-white h:full md:h-screen lg:h-screen xl:h-screeen p-3'>
       <Header />  
-      <div className='flex justify-evenly items-center h-5/6 flex-col-reverse text-white p-6 font-semibold sm:flex-col md:flex-row'>
+      <div className='flex justify-evenly items-center w-full h-5/6 flex-col-reverse p-6 font-semibold md:flex-row'>
         <div className='mt-6 md:mt-2 md:w-4/12'>
-          <h1 className='text-bh-teal font-hello text-6xl md:text-9xl motion-safe:animate-bounce brightness-125'>Hello!</h1>
+          <h1 className='text-bh-teal font-hello text-6xl md:text-8xl xl:text-9xl motion-safe:animate-bounce brightness-125'>Hello!</h1>
           <h3 className='text-pearl text-xl md:text-2xl'>I am Anyanime Benson</h3>
           <h3 className='text-xl text-bh-lime md:text-3xl uppercase font-extrabold mt-3 brightness-125'>Creative Writer <br/> Software Engineer</h3>
           <p className='mt-2 text-xl text-bh-gold'>I harness my creativity as a writer
