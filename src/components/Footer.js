20ceb63a4a0a63ec6c2b71ed8fb1efd399b316c9
Anyanime Benson <bensonanyanime@gmail.com>
2024-01-25 18:42:58 +0100

    Create Footer file

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SocialMediaIcons from "./SocialMediaIcons";
+import Copyright from "./Copyright";
+
+const Footer = () => {
+    return (
+        <div>
+            <SocialMediaIcons />
+            <Copyright />
+        </div>
+    )
+}
+
+export default Footer;
