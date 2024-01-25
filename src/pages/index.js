9d44b205ff863187253844db5eb29015f5351bd4
Anyanime Benson <bensonanyanime@gmail.com>
2024-01-25 18:47:13 +0100

    Refactor code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import Header from '../components/Header';
 import image from '../images/profile.jpeg';
-import SocialMediaIcon from '../components/SocialMediaIcons';
 import Button from '../components/Button';
-import Copyright from '../components/Copyright';
-
+import Layout from './layout';
 
 // This is the index file for the homepage
 
 const index = () => {
   return (
-    <div className='bg-black text-white h:full md:h-screen lg:h-screen xl:h-screeen p-3'>
-      <Header />  
-      <div className='flex justify-evenly items-center w-full h-5/6 flex-col-reverse p-6 font-semibold md:flex-row'>
+    <Layout>
+      <div className='flex justify-evenly items-center flex-col-reverse p-6 font-semibold md:flex-row'>
         <div className='mt-6 md:mt-2 md:w-4/12'>
-          <h1 className='text-bh-teal font-hello text-6xl md:text-8xl xl:text-9xl motion-safe:animate-bounce brightness-125'>Hello!</h1>
+          <h1 className='text-bh-teal font-hello text-5xl md:text-7xl xl:text-8xl motion-safe:animate-bounce brightness-125'>Hello!</h1>
           <h3 className='text-pearl text-xl md:text-2xl'>I am Anyanime Benson</h3>
           <h3 className='text-xl text-bh-lime md:text-3xl uppercase font-extrabold mt-3 brightness-125'>Creative Writer <br/> Software Engineer</h3>
           <p className='mt-2 text-xl text-bh-gold'>I harness my creativity as a writer
@@ -29,18 +25,14 @@ const index = () => {
           <img className='h-full w-96 rounded-full ' src={image} alt='Protrait of Anyanime Benson'/>
         </div>
       </div>
-      <footer className='flex justify-center flex-col gap-2'>
-      <SocialMediaIcon />
-      <Copyright />
-      </footer>
-    </div>
+    </Layout>
   )
 }
 
 //Gatsby Head API
 export const Head = () => <title>Homepage</title>
 
-export default index
+export default index;
 
 
 
